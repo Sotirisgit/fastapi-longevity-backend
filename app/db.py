ec4d23fc2ff4cbdbfd0948eb1efca85342d68ed1
Sotiris <sotpem15@gmail.com>
2025-05-26 23:44:13 +0300

    🔐 Use environment variables for DB connection

--- app/db.py
+++ app/db.py
@@ -1,15 +1,12 @@
 import os
 import psycopg2
-from dotenv import load_dotenv
-
-load_dotenv()
-
-DB_URL = os.getenv("DB_URL")
 
 def get_db_connection():
-    try:
-        conn = psycopg2.connect(DB_URL)
-        return conn
-    except Exception as e:
-        print("Database connection error:", e)
-        return None
+    return psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+    )
+
